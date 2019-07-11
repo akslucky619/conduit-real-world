d3e89fc89ad7032cd78007727e3f08dbf7e991e6
Akash Sharma <akash619@protonmail.com>
2019-07-11 23:59:13 +0530

    added blogpost

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -3,6 +3,37 @@ import React from "react";
 import Header from "./Header";
 
 class Create extends React.Component {
+  state = {
+    title: "",
+    description: "",
+    body: "",
+    tags: []
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleClick = () => {
+    const data = {
+      article: this.state
+    };
+    fetch("https://conduit.productionready.io/api/articles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.token}`
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(article => {
+        console.log(article);
+      });
+  };
+
   render() {
     return (
       <>
@@ -10,15 +41,23 @@ class Create extends React.Component {
         <div className="column is-three-fifths is-offset-one-fifth">
           <div class="field">
             <div class="control">
-              <input class="input" type="text" placeholder="Article Title" />
+              <input
+                onChange={this.handleChange}
+                class="input"
+                type="text"
+                name="title"
+                placeholder="Article Title"
+              />
             </div>
           </div>
 
           <div class="field">
             <div class="control">
               <input
+                onChange={this.handleChange}
                 class="input"
                 type="text"
+                name="description"
                 placeholder="What is this article about?"
               />
             </div>
@@ -27,7 +66,9 @@ class Create extends React.Component {
           <div class="field">
             <div class="control">
               <textarea
+                onChange={this.handleChange}
                 class="textarea"
+                name="body"
                 placeholder="Write your article (in markdown)"
               />
             </div>
@@ -35,13 +76,21 @@ class Create extends React.Component {
 
           <div class="field">
             <div class="control">
-              <input class="input" type="text" placeholder="Input Tags" />
+              <input
+                onChange={this.handleChange}
+                class="input"
+                type="text"
+                name="tags"
+                placeholder="Input Tags"
+              />
             </div>
           </div>
 
           <div class="field is-grouped">
             <div class="control">
-              <button class="button is-link">Submit</button>
+              <button onClick={this.handleClick} class="button is-link">
+                Submit
+              </button>
             </div>
           </div>
         </div>
--- src/Edit.js
+++ src/Edit.js
@@ -1,54 +1,70 @@
 import React from "react";
 import Header from "./Header";
 
-export default function EditUser() {
-  return (
-    <>
-      <Header />
-      <div className="column is-two-fifths is-offset-one-quarter">
-        <div class="field">
-          <div class="control">
-            <input class="input" type="text" placeholder="Article Title" />
+export default class EditUser extends React.Component {
+  handleLogout = () => {
+    localStorage.removeItem("token");
+    this.props.history.push("/");
+  };
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="column is-two-fifths is-offset-one-quarter">
+          <div class="field">
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                placeholder="URL of profile picture"
+              />
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <div class="control">
-            <input
-              class="input"
-              type="text"
-              placeholder="What is this article about?"
-            />
+          <div class="field">
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                placeholder="What is this article about?"
+              />
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <div class="control">
-            <textarea
-              class="textarea"
-              placeholder="Write your article (in markdown)"
-            />
+          <div class="field">
+            <div class="control">
+              <textarea
+                class="textarea"
+                placeholder="Write your article (in markdown)"
+              />
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <div class="control">
-            <input class="input" type="text" placeholder="Input Tags" />
+          <div class="field">
+            <div class="control">
+              <input class="input" type="text" placeholder="Input Tags" />
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <div class="control">
-            <input class="input" type="text" placeholder="Article Title" />
+          <div class="field">
+            <div class="control">
+              <input class="input" type="text" placeholder="Article Title" />
+            </div>
           </div>
-        </div>
 
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button is-link is-right">Submit</button>
+          <div class="field is-grouped">
+            <div class="control">
+              <button class="button is-link is-right">Submit</button>
+              <button
+                onClick={this.handleLogout}
+                class="button is-link is-right"
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
