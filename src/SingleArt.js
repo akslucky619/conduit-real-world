1acde612afea71bdb496e10a1b14c6062394a8ea
Akash Sharma <akash619@protonmail.com>
2019-07-13 20:23:53 +0530

    added object keys

--- src/SingleArt.js
+++ src/SingleArt.js
@@ -40,35 +40,44 @@ class SingleArt extends React.Component {
   };
 
   render() {
+    const { article } = this.state;
+    console.log(article, "check rt");
     return (
       <>
-        <section className="hero is-small is-success is-bold">
-          <div className="hero-body">
-            <div className="container hero-container">
-              <h1 className="title is-1">{this.state.article.title}</h1>
-              <Link to="/profile" className="subtitle is-6">
-                {}
-              </Link>
-            </div>
-          </div>
-        </section>
-        <div class="card">
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <figure class="image is-48x48" />
-                {/* <img /> */}
-                <h2>{}</h2>
+        {Object.keys(article).length ? (
+          <>
+            <section className="hero is-small is-success is-bold">
+              <div className="hero-body">
+                <div className="container hero-container">
+                  <h1 className="title is-1">{this.state.article.title}</h1>
+                  <Link to="/profile" className="subtitle is-6">
+                    {}
+                  </Link>
+                </div>
               </div>
-              <div class="media-content">
-                <p class="title is-4" />
+            </section>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48" />
+                    {/* <img /> */}
+                    <h2>{article.author.username}</h2>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4" />
+                  </div>
+                </div>
+                <div class="content">
+                  <br />
+                </div>
               </div>
             </div>
-            <div class="content">
-              <br />
-            </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          ""
+        )}
+
         {/* {this.state.comments.comments.map((comment, i) => {
           <div class="card">
             <header class="card-header">
